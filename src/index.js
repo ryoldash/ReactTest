1277cba8934f4878a8300226e9b80d9a57e47493
Rashiduddin Yoldash <Rashid@kayten.com.tr>
2018-08-08 17:56:43 +0300

    First exercise completed

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import registerServiceWorker from './registerServiceWorker';
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render( <App /> , document.getElementById('root'));
 registerServiceWorker();
