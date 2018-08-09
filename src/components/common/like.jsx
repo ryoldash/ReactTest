507af4ffa5aaa91a91b65005fe276cbf4363909f
Rashiduddin Yoldash <Rashid@kayten.com.tr>
2018-08-09 08:43:10 +0300

    like component solution completed

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -3,18 +3,17 @@ import React, { Component } from "react";
 // Input: liked: boolean
 // Output: onClick
 
-class Like extends Component {
-  render() {
-    let iconClass = "fa fa-heart";
-    if (this.props.liked) iconClass += "-o";
-    return (
-      <i
-        className={iconClass}
-        aria-hidden="true"
-        style={{ cursor: "pointer" }}
-      />
-    );
-  }
-}
+const Like = props => {
+  let iconClass = "fa fa-heart";
+  if (!props.liked) iconClass += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      className={iconClass}
+      aria-hidden="true"
+      style={{ cursor: "pointer" }}
+    />
+  );
+};
 
 export default Like;
